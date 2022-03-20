90b002a3d62fe85c1f65bdc6ce20c5e45761c2b2
Hai Dang <lehaidangxh@gmail.com>
2022-03-20 10:26:01 +0700

    commit f

---
+++ wavefire/entities/cart.php
@@ -0,0 +1,68 @@
+<?php
+
+session_start();
+
+function getAllStudents()
+{
+    return isset($_SESSION['students']) ? $_SESSION['students'] :array();
+}
+
+function getStudent($student_id)
+{
+    $students = getAllStudents();
+    
+    foreach ($students as $item)
+    {
+        if($item['pro_id'] == $student_id){
+            return $item;
+        }
+    }
+    return array();
+}
+
+function deleteStudent($student_id)
+{
+    $students = getAllStudents();
+
+    foreach($students as $key => $item)
+    {
+        if($item['student_id'] == $student_id){
+            unset($students[$key]);
+        }
+    }
+
+    $_SESSION['students'] = $students;
+
+    return $students;
+}
+
+function updateStudent($student_id, $student_name, $student_email,$img)
+{
+    $students = getAllStudents();
+
+    $new_student = array(
+        'pro_id' => $student_id,
+        'pro_name' => $student_name,
+        'pro_gia' => $student_email,
+       'IMG'=> $img,
+    );
+
+    $is_update_action = false;
+    foreach ($students as $key => $item)
+    {
+        if($item['pro_id'] == $student_id){
+            $students[$key] = $new_student;
+            $is_update_action = true;
+        }
+    }
+
+    if(!$is_update_action){
+        $students[] = $new_student;
+    }
+
+    $_SESSION['students'] = $students;
+
+    return $students;
+}
+
+?>
